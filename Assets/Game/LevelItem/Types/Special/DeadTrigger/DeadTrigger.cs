b56a7d3235f389950519800e622c82bed8a3962e
Andrey Nosov <nosov.and1997@gmail.com>
2024-03-23 01:25:02 +0300

    Level 18

--- Assets/Game/LevelItem/Types/Special/DeadTrigger/DeadTrigger.cs
+++ Assets/Game/LevelItem/Types/Special/DeadTrigger/DeadTrigger.cs
@@ -6,18 +6,25 @@ using UnityEngine;
 public class DeadTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject _deadEffect;
+    [SerializeField] private bool IsOnlyPlayer = false;
 
     private const float DelayDead = 10f;
 
     private void OnTriggerEnter(Collider other)
     {
-        SpawnDeadEffect(other.transform.position);
         if (other.gameObject.tag == Player.PlayerTag)
         {
+            SpawnDeadEffect(other.transform.position);
             other.GetComponent<Player>().Dead();
             return;
         }
 
+        if (IsOnlyPlayer)
+        {
+            return;
+        }
+
+        SpawnDeadEffect(other.transform.position);
         Destroy(other.gameObject);
     }
 
